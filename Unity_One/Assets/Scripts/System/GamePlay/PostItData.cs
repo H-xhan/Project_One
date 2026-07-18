43438290b5cf03239fbaef85a28a591c498ffcb4
H-xhan <khc.cheif@g.shingu.ac.kr>
2026-07-19 03:00:18 +0900

    [Prototype] 포스트잇 추측 데이터 계약 검증

--- Unity_One/Assets/Scripts/System/GamePlay/PostItData.cs
+++ Unity_One/Assets/Scripts/System/GamePlay/PostItData.cs
@@ -262,3 +262,215 @@ public struct PostItWorldDropData :
         }
     }
 }
+
+[Serializable]
+public enum PostItGuessStatus
+{
+    None = 0,
+    Pending = 1,
+    Submitted = 2,
+    Correct = 3,
+    Incorrect = 4,
+    Skipped = 5
+}
+
+[Serializable]
+public struct PostItGuessOwnerData :
+    INetworkSerializable,
+    IEquatable<PostItGuessOwnerData>
+{
+    public int RoundRevision;
+    public int GuessRevision;
+    public int PostItId;
+    public int VisualId;
+    public PostItTopicId SelectedTopicId;
+    public PostItTopicId RevealedTopicId;
+    public PostItGuessStatus Status;
+
+    public bool IsValid =>
+        RoundRevision >= 0 &&
+        GuessRevision >= 0 &&
+        PostItId >= 0 &&
+        VisualId > 0 &&
+        Status != PostItGuessStatus.None;
+
+    public static PostItGuessOwnerData Invalid => new PostItGuessOwnerData(
+        -1,
+        -1,
+        -1,
+        -1,
+        PostItTopicId.None,
+        PostItTopicId.None,
+        PostItGuessStatus.None);
+
+    public PostItGuessOwnerData(
+        int roundRevision,
+        int guessRevision,
+        int postItId,
+        int visualId,
+        PostItTopicId selectedTopicId,
+        PostItTopicId revealedTopicId,
+        PostItGuessStatus status)
+    {
+        RoundRevision = roundRevision;
+        GuessRevision = guessRevision;
+        PostItId = postItId;
+        VisualId = visualId;
+        SelectedTopicId = selectedTopicId;
+        RevealedTopicId = revealedTopicId;
+        Status = status;
+    }
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref RoundRevision);
+        serializer.SerializeValue(ref GuessRevision);
+        serializer.SerializeValue(ref PostItId);
+        serializer.SerializeValue(ref VisualId);
+        serializer.SerializeValue(ref SelectedTopicId);
+        serializer.SerializeValue(ref RevealedTopicId);
+        serializer.SerializeValue(ref Status);
+    }
+
+    public bool Equals(PostItGuessOwnerData other)
+    {
+        return RoundRevision == other.RoundRevision &&
+               GuessRevision == other.GuessRevision &&
+               PostItId == other.PostItId &&
+               VisualId == other.VisualId &&
+               SelectedTopicId == other.SelectedTopicId &&
+               RevealedTopicId == other.RevealedTopicId &&
+               Status == other.Status;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is PostItGuessOwnerData other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 31 + RoundRevision;
+            hash = hash * 31 + GuessRevision;
+            hash = hash * 31 + PostItId;
+            hash = hash * 31 + VisualId;
+            hash = hash * 31 + (int)SelectedTopicId;
+            hash = hash * 31 + (int)RevealedTopicId;
+            hash = hash * 31 + (int)Status;
+            return hash;
+        }
+    }
+}
+
+[Serializable]
+public struct PostItGuessPlayerScoreData :
+    INetworkSerializable,
+    IEquatable<PostItGuessPlayerScoreData>
+{
+    public int RoundRevision;
+    public int GuessRevision;
+    public ulong OwnerClientId;
+    public int HeldPostItCount;
+    public int EligibleCount;
+    public int SubmittedCount;
+    public int CorrectCount;
+    public int GuessBonusScore;
+    public int FinalRoundScore;
+
+    public bool IsValid =>
+        RoundRevision >= 0 &&
+        GuessRevision >= 0 &&
+        OwnerClientId != ulong.MaxValue &&
+        HeldPostItCount >= 0 &&
+        EligibleCount >= 0 &&
+        SubmittedCount >= 0 &&
+        SubmittedCount <= EligibleCount &&
+        CorrectCount >= 0 &&
+        CorrectCount <= SubmittedCount &&
+        GuessBonusScore >= 0;
+
+    public static PostItGuessPlayerScoreData Invalid => new PostItGuessPlayerScoreData(
+        -1,
+        -1,
+        ulong.MaxValue,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0);
+
+    public PostItGuessPlayerScoreData(
+        int roundRevision,
+        int guessRevision,
+        ulong ownerClientId,
+        int heldPostItCount,
+        int eligibleCount,
+        int submittedCount,
+        int correctCount,
+        int guessBonusScore,
+        int finalRoundScore)
+    {
+        RoundRevision = roundRevision;
+        GuessRevision = guessRevision;
+        OwnerClientId = ownerClientId;
+        HeldPostItCount = heldPostItCount;
+        EligibleCount = eligibleCount;
+        SubmittedCount = submittedCount;
+        CorrectCount = correctCount;
+        GuessBonusScore = guessBonusScore;
+        FinalRoundScore = finalRoundScore;
+    }
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref RoundRevision);
+        serializer.SerializeValue(ref GuessRevision);
+        serializer.SerializeValue(ref OwnerClientId);
+        serializer.SerializeValue(ref HeldPostItCount);
+        serializer.SerializeValue(ref EligibleCount);
+        serializer.SerializeValue(ref SubmittedCount);
+        serializer.SerializeValue(ref CorrectCount);
+        serializer.SerializeValue(ref GuessBonusScore);
+        serializer.SerializeValue(ref FinalRoundScore);
+    }
+
+    public bool Equals(PostItGuessPlayerScoreData other)
+    {
+        return RoundRevision == other.RoundRevision &&
+               GuessRevision == other.GuessRevision &&
+               OwnerClientId == other.OwnerClientId &&
+               HeldPostItCount == other.HeldPostItCount &&
+               EligibleCount == other.EligibleCount &&
+               SubmittedCount == other.SubmittedCount &&
+               CorrectCount == other.CorrectCount &&
+               GuessBonusScore == other.GuessBonusScore &&
+               FinalRoundScore == other.FinalRoundScore;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is PostItGuessPlayerScoreData other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 31 + RoundRevision;
+            hash = hash * 31 + GuessRevision;
+            hash = hash * 31 + OwnerClientId.GetHashCode();
+            hash = hash * 31 + HeldPostItCount;
+            hash = hash * 31 + EligibleCount;
+            hash = hash * 31 + SubmittedCount;
+            hash = hash * 31 + CorrectCount;
+            hash = hash * 31 + GuessBonusScore;
+            hash = hash * 31 + FinalRoundScore;
+            return hash;
+        }
+    }
+}
